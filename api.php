45c3c9497e0ef41f23d3387d4d471851112ad1de
Boldijar Paul <boldijar.paul@gmail.com>
2016-01-26 10:25:50 -0800

    added comments

--- api.php
+++ api.php
@@ -8,13 +8,16 @@ use Symfony\Component\HttpFoundation\Response;
 $app = new Silex\Application();
 
 $app->post('/upload', function (Request $request) use ($app){
+	// get file from requests
 	$file = $request->files->get('image');
+	// if null return error
 	if ($file==null) {
 		$obj=new stdClass();
 		$obj->success=false;
 		$obj->error="No image provided";
 	 	return json_encode($obj);
 	}
+	// upload the file and return the json with the data
 	return json_encode(FileHelper::writeFile($file));
 });
 $app->run();
