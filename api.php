5267ff5e40df5f79440b0d9eff404e1c60148e73
Boldijar Paul <boldijar.paul@gmail.com>
2016-01-26 06:43:46 -0800

    doing upload on /upload now

--- api.php
+++ api.php
@@ -2,51 +2,21 @@
 require_once 'vendor/autoload.php';
 require_once 'FileHelper.php';
 
-
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 $app = new Silex\Application();
 
-
-$app->get('/hello', function ( Application $app, Request $request) use ($app) {
-	return "Sup";
-});
-
-$app->get('/version', function ( Application $app, Request $request) use ($app) {
-	$obj = new stdClass();
-  $obj->name="1.0.0";
-  $obj->stable=true;
-  return json_encode($obj);
-});
-
 $app->post('/upload', function (Request $request) use ($app){
 	$file = $request->files->get('image');
 	if ($file==null) {
-		return "no file";
+		$obj=new stdClass();
+		$obj->success=false;
+		$obj->error="No image provided";
+	(	return json_encode($obj);
 	}
-	FileHelper::writeFile($file);
-	return "done";
+	return json_encode(FileHelper::writeFile($file));
 });
-
-// $app->get('/suggestion', function ( Application $app, Request $request) use ($app) {
-// 		$question= $request->query->get('question');
-// 		global $answerController;
-// 		return createResponse($answerController->getAnswer($question));
-// });
-
-
-function createResponse($object){
-		$response = new Response();
-		$response->setContent(json_encode($object));
-		$response->setStatusCode(200);
-	  $response->headers->set("Access-Control-Allow-Origin","*");
-    $response->headers->set("Access-Control-Allow-Methods","GET,POST,PUT,DELETE,OPTIONS");
-    $response->headers->set("Content-Type","application/json; charset=UTF-8 ");
-    return $response;
-}
-
-
 $app->run();
 
 ?>
